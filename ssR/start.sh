537bccd20ec66b81bb4e108d98b082040fcb4c8e
szclsya <szclsya@gmail.com>
2016-09-04 21:56:14 +0800

    Turn to use config file.

--- ssR/start.sh
+++ ssR/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "========================================================================"
-echo " Your shadowsocksR server information"
-echo ""
-echo " server: $SS_SERVER_ADDR  port: $SS_SERVER_PORT password: $SS_PASSWORD  "
-echo " timeout: $SS_TIMEOUT  encryption method: $SS_METHOD "
-echo ""
-echo " Please remember the password!"
-echo "========================================================================"
-
 ulimit -n 512000
 python ./server.py
